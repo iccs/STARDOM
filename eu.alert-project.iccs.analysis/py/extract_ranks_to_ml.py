d7666fc00a8dce5360ee88fb06d6a29072040003
Fotis Paraskevopoulos <fotis@existanze.com>
2012-01-30 16:06:18 +0200

    Adding script to create initial classification

---
+++ eu.alert-project.iccs.analysis/py/extract_ranks_to_ml.py
@@ -0,0 +1,177 @@
+from datetime import datetime
+import sys
+import MySQLdb
+import config
+
+__author__ = 'fotis'
+
+
+
+def get_connection():
+    conn = MySQLdb.connect (
+                                host = 'localhost',
+                                port = 8889,
+                                user = 'alert',
+                                passwd = '1234',
+                                db  = 'alert_historical')
+    return conn
+
+
+#
+# Attemps to return the most suitable name
+# for printing, which includes both
+# the first name and the lastname
+#
+#
+def get_name_and_lastname(id):
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM identity_profile_view WHERE id=%s",id)
+    results= cursor.fetchall()
+
+    ret = ''
+
+    for row in results:
+
+        name =      row[3].strip() if row[3] is not None else ""
+        lastname =  row[4].strip() if row[4] is not None else ""
+        username =  row[5].strip() if row[5] is not None else ""
+        email =     row[6].strip() if row[6] is not None else ""
+
+        if name != '' and lastname != '' :
+            ret  = name+" "+lastname
+            break
+        elif lastname == '' and  name !='' and email !='' :
+            ret = name+" "+email
+        elif lastname == '' and name == '' and email !='' and username !='' :
+            ret = email +" "+username
+        elif lastname == '' and name == '' and email =='' and username !='' :
+            ret = username
+        else:
+            ret = email
+
+    conn.close()
+    return ret
+
+
+#
+# Creates the analogous ranking for the developer in the identity
+# mpa
+#
+def create_rankings(file):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    identities = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6
+    ]
+
+    tables = [
+        "scm_activity_metric",
+        "scm_api_introduced_metric",
+        "its_activity_metric",
+        "mailing_list_activity_metric"
+    ]
+
+
+    result={}
+
+    for value in tables:
+
+        print "Looking for metric %s " % value
+
+        rankings_query = """
+        select identity_id, count(id) as activity
+        from %s join metric using(id) group by identity_id order by activity DESC
+        limit %s;""" % (value,config.extract_max_search_results)
+
+
+        cursor.execute(rankings_query)
+        results = cursor.fetchall()
+
+
+        for row in results:
+
+            identity_id = row[0]
+            metric_value = row[1]
+
+            if not result.has_key(identity_id):
+                result[identity_id]={}
+
+            if not result[identity_id].has_key(value):
+                result[identity_id][value] = []
+
+            result[identity_id][value].extend([metric_value])
+
+
+
+
+        #create the file
+    output_file="%s/%s.arff" % (file,"rankings-"+datetime.now().strftime("%Y%m%d%H%M%S"))
+    print "Creating output file %s" % output_file;
+
+    bugAWriter = open(output_file, 'wb')
+    bugAWriter.writelines((
+                              """ % 1.  Title: Bug Solution Dataset Numeric
+                            %
+                            % 2. Sources:
+                            %(a) Creator: ALERT Project
+                            %(b) Date: """+(datetime.now().strftime("%d %b %Y - %H:%M:%S")))+"""\n%\n\n""")
+
+    bugAWriter.writelines("@RELATION bug\n")
+    bugAWriter.writelines("\n\n\n")
+    bugAWriter.writelines("@ATTRIBUTE identity_id  NUMERIC\n")
+
+
+    # Go over the enabled metrics and create the heading
+    for value in tables:
+        bugAWriter.writelines("@ATTRIBUTE %s NUMERIC\n" % value)
+
+
+    bugAWriter.writelines("@ATTRIBUTE relevant {TRUE,FALSE}\n")
+    bugAWriter.writelines("\n@DATA\n")
+
+
+    #print result
+    for identity_id, metrics in result.iteritems():
+
+        row = [identity_id]
+
+        for value in tables:
+
+            value = metrics[value] if metrics.has_key(value) else [0]
+            row.extend(value)
+
+
+        row.append(("TRUE" if identity_id in identities else "FALSE"))
+
+
+        bugAWriter.writelines(', '.join(str(x) for x in row)+"\n")
+
+
+    bugAWriter.close()
+    conn.close()
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print """
+            You need to specify a directory as an argument
+        """
+        exit(1)
+
+
+    print  "Extracting information to %s " % sys.argv[1]
+
+    create_rankings(sys.argv[1])
+
+
