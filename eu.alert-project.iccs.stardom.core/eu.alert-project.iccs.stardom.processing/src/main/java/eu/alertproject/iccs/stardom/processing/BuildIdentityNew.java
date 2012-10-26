825bcd987cab2260420f5b13ff2a0ad03f2a946a
Fotis Paraskevopoulos <fotis@existanze.com>
2012-10-27 01:49:26 +0300

    Fixing small issue with the e-mail sluggification

--- eu.alert-project.iccs.stardom.core/eu.alert-project.iccs.stardom.processing/src/main/java/eu/alertproject/iccs/stardom/processing/BuildIdentityNew.java
+++ eu.alert-project.iccs.stardom.core/eu.alert-project.iccs.stardom.processing/src/main/java/eu/alertproject/iccs/stardom/processing/BuildIdentityNew.java
@@ -1,7 +1,5 @@
 package eu.alertproject.iccs.stardom.processing;
 
-import com.sun.tools.doclets.internal.toolkit.util.SourceToHTMLConverter;
-import eu.alertproject.iccs.stardom.datastore.api.dao.IdentityDao;
 import eu.alertproject.iccs.stardom.processing.services.IdentityService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
